727551ef76f64f46675ff72c6bd51ee735048728
Troydavisinf <troy.davis@infinitaz.com>
2025-04-01 20:51:32 -0500

    Prob001 & Easy Section Started

---
+++ prob001.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+public class prob001 {
+
+    public static void main(String[] args) throws Exception {
+        new prob001().run();
+    }
+
+    public void run() throws Exception {
+
+        System.out.println("   []\n" +
+                "   []\n" +
+                "   []");
+
+    }
+}
